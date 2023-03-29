9fe5181012a2345423f9c63e3191ff7883fd26d4
TechGun <96291700+TechGun@users.noreply.github.com>
2023-03-29 11:22:32 +0530

    part-6

---
+++ Part-6/variable.py
@@ -0,0 +1,21 @@
+text = False
+
+# print("student")
+# print(text)
+
+text = False
+text1 = 5
+Text = 6
+
+first_name = "vishwajeet"
+
+
+name = "vishwajeet"
+Name = "Ajeet"
+
+print(Name)
+
+
+pass_exam = 6
+
+age = 6
