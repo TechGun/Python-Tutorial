d1661de6b8fe1e8c8cab2e6065ce26d9d9fe9cbc
TechGun <96291700+TechGun@users.noreply.github.com>
2023-04-12 00:13:55 +0530

    part-7

---
+++ Part-7/string.py
@@ -0,0 +1,18 @@
+
+# name = 'vishwajeet'
+# name = "vishwajeet"
+
+# name = """vishwajeet
+#
+#          kumar"""
+
+name = "vishwajeet"
+# length = len(name)
+# print(name[3])
+# print(name[-2])
+
+print(name[0:3])
+print(name[2:5])
+print(name[2:])
+print(name[:5])
+print(name[:])
