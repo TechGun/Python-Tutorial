39565dc6db782cd7755e142015b90a37d3e83cef
TechGun <96291700+TechGun@users.noreply.github.com>
2023-04-13 23:15:51 +0530

    part-11

---
+++ Part-11/numbers.py
@@ -0,0 +1,17 @@
+num1 = 7  # integer
+
+num2 = 2.5  # float
+
+result = num1 + num2  # add
+result = num1 - num2  # subtract
+result = num1 * num2  # multiply
+result = num1 / num2  # division
+result = num1 // num2  # when two int are divided result in int
+result = num1 ** num2  # power
+result = num1 % num2  # remainder
+
+print(result)
+
+num1 *= 3  # shortcut of num1 = num1 + 3
+
+print(num1)
---
+++ Part-11/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+num1 *= 3  # shortcut of num1 = num1 + 3
+
+print(num1)
