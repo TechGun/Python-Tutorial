a844adeb8ae0a673e6122e75d3066a67ce5da91e
TechGun <96291700+TechGun@users.noreply.github.com>
2023-04-24 13:39:28 +0530

    part-15

---
+++ Part-15/implicit.py
@@ -0,0 +1,17 @@
+# x = 5  # int 5.0 float
+# y = 2.5  # float
+
+# sum = x + y
+
+# print(sum)
+
+# print(type(sum))
+
+x = 5  # int
+y = 2  # int
+
+result = x/y
+
+print(result)
+
+print(type(result))
