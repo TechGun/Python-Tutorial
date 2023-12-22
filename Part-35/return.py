a44eedb2009eba598a817ba35da4152df63c7f75
TechGun <96291700+TechGun@users.noreply.github.com>
2023-12-22 14:28:57 +0530

    part-36

---
+++ Part-35/return.py
@@ -0,0 +1,27 @@
+# def print_add(num1, num2):
+#     result = num1+num2
+#     print(result)
+
+
+# print_add(2, 3)
+
+# def return_add(num1, num2):
+#     result = num1+num2
+#     return result
+
+
+# r = return_add(2, 3)
+
+# print(r)
+
+def return_two(num1, num2):
+    r1 = num1*2
+    r2 = num2*3
+    return r1, r2
+
+
+# print(return_two(2, 3))
+
+res1, res2 = return_two(2, 3)
+print(res1)
+print(res2)
---
+++ Part-36/args.py
@@ -0,0 +1,9 @@
+def add(*num):
+    result = 0
+    for n in num:
+        result = result + n
+
+    print(result)
+
+
+add(2, 3, 5, 7)
