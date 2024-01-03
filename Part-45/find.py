1d4a68066f54608ec11c305a87fc9d574f3fce98
TechGun <96291700+TechGun@users.noreply.github.com>
2024-01-03 14:13:44 +0530

    part-45

---
+++ Part-45/find.py
@@ -0,0 +1,9 @@
+names = ["vishwajeet", "ajit", "india", "techgun", "ajit"]
+
+# if "ajit" in names:
+#     pos = names.index("ajit")
+#     print(pos)
+# else:
+#     print("no")
+
+print(names.count("vishwajit"))
